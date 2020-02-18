9a94fbbcfb109971adfebb8afb31b779b4c8ed8c
yeLyn36 <s2018w11@e-mirim.hs.kr>
2020-02-19 07:08:51 +0900

    add readPackListbySetting

--- routes/packlist.js
+++ routes/packlist.js
@@ -4,7 +4,14 @@ var db = require("../db");
 const router = express.Router();
 
 //TODO: theme, gender, weather에 따라 가져오는 JSON 값이 다름.
-router.get("/readListAll", (req, res) => {
+router.get("/readPackListbySetting/:theme/:gender/:weather", (req, res) => {
+  var theme = req.params.theme;
+  var gender = req.params.gender;
+  var weather = req.params.weather;
+
+  if (theme.length != 0) const themeArr = theme.split(" ");
+  if (weather.length != 0) const weatherArr = weather.split(" ");
+
   db((err, conn) => {
     if (err) throw err;
     var sql = "SELECT * FROM pack_list";
