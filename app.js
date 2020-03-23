9ea1cec8832eac69e3d9d1dde6f3b4b94fa542f7
yeLyn36 <s2018w11@e-mirim.hs.kr>
2020-03-23 21:25:00 +0900

    add checklist router

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.set("view engine", "jade");
 
 app.use(logger("dev"));
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
--- routes/checklist.js
+++ routes/checklist.js
@@ -41,46 +41,51 @@ router.get("/readSelectedList/:title", (req, res) => {
   });
 });
 
-router.post("/addList", (req, res) => {
-  ///:title/:city/:start_date/:finish_date/:uid/:theme
-  
-  let title = req.params.title;
-  let city = req.params.city;
-  let start_date = req.params.start_date;
-  let finish_date = req.params.finish_date;
-  let uid = req.params.uid;
-  let theme = req.params.theme;
+router.post(
+  "/addList/:title/:city/:start_date/:finish_date/:uid/:theme",
+  (req, res) => {
+    ///:title/:city/:start_date/:finish_date/:uid/:theme
 
-  // let title = req.query.title;
-  // let city = req.query.city;
-  // let start_date = req.query.start_date;
-  // let finish_date = req.query.finish_date;
-  // let uid = req.query.uid;
-  // let theme = req.query.theme;
+    console.log(req.params);
 
-  let insertData = {
-    num: 0,
-    title: title,
-    city: city,
-    start_date: start_date,
-    finish_date: finish_date,
-    uid: uid,
-    theme: theme
-  };
+    let title = req.params.title;
+    let city = req.params.city;
+    let start_date = req.params.start_date;
+    let finish_date = req.params.finish_date;
+    let uid = req.params.uid;
+    let theme = req.params.theme;
 
-  db((err, conn) => {
-    if (err) {
-      throw err;
-    }
-    let sql = "INSERT INTO check_list SET ?;";
-    conn.query(sql, insertData, (err, result) => {
+    // let title = req.query.title;
+    // let city = req.query.city;
+    // let start_date = req.query.start_date;
+    // let finish_date = req.query.finish_date;
+    // let uid = req.query.uid;
+    // let theme = req.query.theme;
+
+    let insertData = {
+      num: 0,
+      title: title,
+      city: city,
+      start_date: start_date,
+      finish_date: finish_date,
+      uid: uid,
+      theme: theme
+    };
+
+    db((err, conn) => {
       if (err) {
         throw err;
       }
-      res.send(result);
+      let sql = "INSERT INTO check_list SET ?;";
+      conn.query(sql, insertData, (err, result) => {
+        if (err) {
+          throw err;
+        }
+        res.send(result);
+      });
     });
-  });
-});
+  }
+);
 
 router.post("/updateSelectedList/", (req, res) => {});
 
