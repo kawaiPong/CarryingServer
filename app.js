15bec7f3e19f80231ca3d61cf2072b120068204a
yeLyn36 <s2018w11@e-mirim.hs.kr>
2020-04-24 02:38:05 +0900

    setting prettier

--- app.js
+++ app.js
@@ -1,30 +1,30 @@
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
-var logger = require("morgan");
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
 
-var indexRouter = require("./routes/index");
-var userRouter = require("./routes/user");
-var itemRouter = require("./routes/checkListItem");
-var listRouter = require("./routes/checkList");
+var indexRouter = require('./routes/index');
+var userRouter = require('./routes/user');
+var itemRouter = require('./routes/checkListItem');
+var listRouter = require('./routes/checkList');
 
 var app = express();
 
 // view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
-app.use(logger("dev"));
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use("/", indexRouter);
-app.use("/user", userRouter);
+app.use('/', indexRouter);
+app.use('/user', userRouter);
 //회원 관련 기능
-app.use("/list", listRouter);
+app.use('/list', listRouter);
 //체크리스트의 목록 기능
-app.use("/item", itemRouter);
+app.use('/item', itemRouter);
 //하나의 체크리스트 세분화된 기능
 
 app.use(function (req, res, next) {
--- db/config.js
+++ db/config.js
@@ -1,14 +1,15 @@
-const mysql = require("mysql");
+const mysql = require('mysql');
 
 const dbConfig = {
-  host: "ec2-15-164-215-173.ap-northeast-2.compute.amazonaws.com",
-  user: "ec2-user",
-  password: "carrying",
-  database: "carrying"
-  // host: "localhost",
-  // user: "root",
-  // password: "1234",
-  // database: "carrying"
+  host: 'ec2-15-164-215-173.ap-northeast-2.compute.amazonaws.com',
+  user: 'ec2-user',
+  password: 'carrying',
+  database: 'carrying',
+  multipleStatements: true,
+  // host: 'localhost',
+  // user: 'root',
+  // password: '1234',
+  // database: 'carrying',
 };
 
 module.exports = dbConfig;
