37b0eb173c680f31e9e9f4a4c6210df92e701dca
yeLyn36 <s2018w11@e-mirim.hs.kr>
2020-04-24 02:39:27 +0900

    add router create, add, delete successfully

--- routes/checkLIstItem.js
+++ routes/checkLIstItem.js
@@ -1,62 +1,46 @@
-const express = require("express");
-const db = require("../db/db");
+const express = require('express');
+const db = require('../db/db');
 const router = express.Router();
-
-//DONE:생성 시 체크리스트의 테마에 맞게 물건 불러오기
-/* SELECT * FROM PACKLIST WHERE THEME = ? */
-
-//체크리스트 저장 -> json 처리, 처음엔 저장, 이후엔 편집이 되어야 하는데 이는 어떻게 할 것인가요 ???
-/* INSERT INTO CHECKLISTITEM VALUES (0, NAME, THEME, ...) */
 
 //체크리스트 읽기
 /* SELECT NAME, STATUS FROM CHECKLISTITEM WHERE NUM = ? */
 
-//TODO:불러온 상태로 편집할 때 -> 추가할 때, 삭제할 때
-/* 추가
-   INSERT INTO */
-
 //해당 물건 체크 및 미체크
 /* UPDATE CHECKLISTITEM SET STATE = ? WHERE NUM = ?  */
 
-//체크리스트 삭제
-/* DELETE * FROM CHECKLISTITEM WHERE NUM = ? */
+//DONE:생성 시 체크리스트의 테마에 맞게 물건 불러오고 저장
+router.post(
+  '/createCheckList/:list_num/:theme/:gender/:weather',
+  (req, res) => {
+    let listnum = req.params.list_num;
+    let theme = req.params.theme;
+    let gender = req.params.gender;
+    let weather = req.params.weather;
+    let sql;
 
-router.get("/createCheckList/:theme", (req, res) => {
-  let theme = req.params.theme;
-  let sql;
+    //   if (theme.length != 0) {
+    //     const themeArr = theme.split(" ");
+    //   }
+    //   if (weather.length != 0) {
+    //     const weatherArr = weather.split(" ");
+    //   }
 
-  db((err, conn) => {
-    if (err) throw err;
-    sql = "SELECT name FROM PACK_LIST WHERE THEME = ?";
-    conn.query(sql, theme, (err, rows) => {
+    db((err, conn) => {
       if (err) throw err;
-      result = rows;
-      console.log(rows);
-      res.send(rows);
+      sql =
+        'insert into check_list_item select 0, name, false, ? from pack_list where theme = ? or gender = ? or weather = ?;'; //or gender = ? or weather or ? ;
+      conn.query(sql, [listnum, theme, gender, weather], (err, rows) => {
+        //gender, weather 포함
+        if (err) throw err;
+        result = rows;
+        console.log(rows);
+        res.send(rows);
+      });
     });
-  });
-});
+  }
+);
 
-//생성된 체크리스트 및 수정된 체크리스트 저장
-
-//TODO: json 형식을 어떻게 가져오게 할까 .
-router.post("/saveCheckListItem/:uid/:checkListNum/:...", (req, res) => {
-  let uid = req.params.uid;
-  let checkListNum = req.params.checkListNum;
-
-  db((err, conn) => {
-    if (err) throw err;
-    sql = "INSERT INTO check_list_item values (0, ...)";
-    conn.query(sql, theme, (err, rows) => {
-      if (err) throw err;
-      result = rows;
-      console.log(rows);
-      res.send(rows);
-    });
-  });
-});
-
-router.get("/readCheckListItems/:uid/:checkListNum", (req, res) => {
+router.get('/readCheckListItems/:uid/:checkListNum', (req, res) => {
   let uid = req.params.uid;
   let checkListNum = req.params.checkListNum;
 
@@ -64,7 +48,7 @@ router.get("/readCheckListItems/:uid/:checkListNum", (req, res) => {
     if (err) throw err;
 
     let sql =
-      "select name, status from check_list_item where uid = ? and checkListNum = ?";
+      'select name, status from check_list_item where uid = ? and checkListNum = ?';
     conn.query(sql, [uid, checkListNum], (err, result) => {
       if (err) throw err;
       res.send(result[0]);
@@ -72,47 +56,36 @@ router.get("/readCheckListItems/:uid/:checkListNum", (req, res) => {
   });
 });
 
-router.post(
-  "/addCheckListItem/:uid/:checkListNum/:name/:status",
-  (req, res) => {
-    let uid = req.params.uid;
-    let checkListNum = req.params.checkListNum;
-    let name = req.params.name;
-    let status = req.params.status;
+//항목 추가
+router.post('/addCheckListItem/:listNum/:name', (req, res) => {
+  let listNum = req.params.listNum;
+  let name = req.params.name;
 
-    db((err, conn) => {
+  db((err, conn) => {
+    if (err) throw err;
+    sql = 'INSERT INTO check_list_item values (0, ?, false, ?)';
+    conn.query(sql, [name, listNum], (err, rows) => {
       if (err) throw err;
-      sql = "INSERT INTO check_list_item values (0, ...)";
-      conn.query(sql, theme, (err, rows) => {
-        if (err) throw err;
-        res.send(rows);
-      });
+      res.send(rows);
     });
-  }
-);
+  });
+});
 
-// //TODO: theme, gender, weather에 따라 가져오는 JSON 값이 다름.
-// router.get("/readPackListbySetting/:theme/:gender/:weather", (req, res) => {
-//   var theme = req.params.theme;
-//   var gender = req.params.gender;
-//   var weather = req.params.weather;
+// check_list의 num에 check_list_item의 list_num을 외래키로 걸어
+// on delete cascade를 설정했기에 앞 check_list가 사라지면 해당 check_list_itme 레코드들도 다 삭제됨.
+//항목 삭제
+router.post('/deleteCheckListItem/:list_num/:name', (req, res) => {
+  db((err, conn) => {
+    if (err) throw err;
 
-//   if (theme.length != 0) {
-//     const themeArr = theme.split(" ");
-//   }
-//   if (weather.length != 0) {
-//     const weatherArr = weather.split(" ");
-//   }
+    let sql =
+      'delete from check_list_item where num = (select * from (select num from user where name = ? and list_num = ?) as t)';
+    conn.query(sql, nickname, (err, result) => {
+      if (err) throw err;
+      console.log(result);
+      res.send(result);
+    });
+  });
+});
 
-//   db((err, conn) => {
-//     if (err) throw err;
-//     var sql = "SELECT * FROM pack_list";
-//     conn.query(sql, (err, rows) => {
-//       if (err) throw err;
-//       return res.json({ data: rows });
-//     });
-//   });
-// });
-
-router.post("/deleteCheckListItem/:num", (req, res) => {});
 module.exports = router;
