a3c04fddf3de699806aa02799cf6edba745b65aa
yeLyn36 <s2018w11@e-mirim.hs.kr>
2020-05-30 02:03:50 +0900

    set timeout

--- db/db.js
+++ db/db.js
@@ -1,24 +1,34 @@
-const mysql = require("mysql");
-const config = require("./config");
-const logger = require("./logger");
+const mysql = require('mysql');
+const config = require('./config');
+const logger = require('./logger');
 
-var pool = mysql.createPool(config);
-logger.info("Connection pool created.");
+var pool = mysql.createPool({
+  connectionLimit: 1000,
+  connectTimeout: 60 * 60 * 1000,
+  acquireTimeout: 60 * 60 * 1000,
+  timeout: 60 * 60 * 1000,
+  host: config.host,
+  user: config.user,
+  password: config.password,
+  database: config.database,
+});
 
-pool.on("acquire", function(connection) {
+logger.info('Connection pool created.');
+
+pool.on('acquire', function (connection) {
   logger.info(`Connection ${connection.threadId} acquired`);
 });
 
-pool.on("enqueue", function() {
-  logger.info("Waiting for available connection slot");
+pool.on('enqueue', function () {
+  logger.info('Waiting for available connection slot');
 });
 
-pool.on("release", function(connection) {
+pool.on('release', function (connection) {
   logger.info(`Connection ${connection.threadId} released`);
 });
 
-const getConn = function(callback) {
-  pool.getConnection(function(err, conn) {
+const getConn = function (callback) {
+  pool.getConnection(function (err, conn) {
     callback(err, conn);
     conn.release(); // 연결세션 반환.
   });
