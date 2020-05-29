aa39960658ed5b0e8a1ea45fb49ce5d8654df5a2
yeLyn36 <s2018w11@e-mirim.hs.kr>
2020-05-30 02:11:14 +0900

    set config

--- db/config.js
+++ db/config.js
@@ -1,13 +1,13 @@
 const dbConfig = {
-  // host: 'ec2-15-164-215-173.ap-northeast-2.compute.amazonaws.com',
-  // user: 'ec2-user',
-  // password: 'carrying',
-  // database: 'carrying',
-  // multipleStatements: true,
-  host: 'localhost',
-  user: 'root',
+  host: 'ec2-54-180-82-41.ap-northeast-2.compute.amazonaws.com',
+  user: 'ec2-user',
   password: 'carrying',
   database: 'carrying',
+  multipleStatements: true,
+  // host: 'localhost',
+  // user: 'root',
+  // password: '1234',
+  // database: 'carrying',
 };
 
 module.exports = dbConfig;
