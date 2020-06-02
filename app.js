eb60c1e5f2f3acf847836d4a371cd2b376dbd2c1
yeLyn36 <s2018w11@e-mirim.hs.kr>
2020-06-03 02:07:25 +0900

    modified

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var logger = require('morgan');
 
-var userRouter = require('./routes/index');
+var indexRouter = require('./routes/index');
 var userRouter = require('./routes/user');
 var itemRouter = require('./routes/checkListItem');
 var listRouter = require('./routes/checkList');
