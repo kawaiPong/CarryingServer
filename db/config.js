e86b2c57ea59b6b4c8d60ac51afe17e3219da302
yerin <ghddnjf2901@gmail.com>
2020-07-07 15:47:19 +0900

    change host

--- db/config.js
+++ db/config.js
@@ -1,14 +1,14 @@
 const dbConfig = {
-  // host: 'localhost',
-  // user: 'root',
-  // password: 'carryingserver',
-  // database: 'carrying',
-  // multipleStatements: true,
   host: 'localhost',
   user: 'root',
-  password: '1234',
+  password: 'carryingserver',
   database: 'carrying',
   multipleStatements: true,
+  // host: 'localhost',
+  // user: 'root',
+  // password: '1234',
+  // database: 'carrying',
+  // multipleStatements: true,
 };
 
 module.exports = dbConfig;
